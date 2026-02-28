4d91ef96a42fc182c4f26b6899631e292860d277
bouncytorch <bouncytorch@aurigrave.org>
2026-02-28 12:12:55 +0100

    Added cmayk's website to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import YouTubeFrame from '@/components/ui/frame/YouTubeFrame';
 import { LINKTREE_LINKS } from '@/lib/const';
 
 import styles from './page.module.css';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
     return (
@@ -15,12 +16,15 @@ export default function Home() {
             <div className={styles.body}>
                 <main className={styles.main}>
                     <h1><Logo /></h1>
-                    <p>bouncytorch&apos;s linktree and portfolio page</p>
+                    <p>currently serving as bouncytorch&apos;s linktree and portfolio page</p>
                     <NavButtons iconBreakpoint='300px' />
                     <p>portfolio reel:</p>
                     <YouTubeFrame link="https://www.youtube.com/embed/1jF1c8nP-JY"/>
                     <p>links:</p>
+                    <h3>bouncytorch</h3>
                     <Linktree links={LINKTREE_LINKS} />
+                    <h3>cmayk</h3>
+                    <Linktree links={[{ name: 'oxirian.net', icon: faGlobe, link: 'https://oxirian.net'  }]}></Linktree>
                     <ThemeButton />
                     <Footer />
                 </main>
