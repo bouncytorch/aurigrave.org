832de66273df1df54c93e826d1446e9ba3ba4410
bouncytorch <bouncytorch@aurigrave.org>
2026-03-18 18:19:37 +0100

    Forgot a cover_url in one of the components

--- src/components/ui/button/ReleaseCard.tsx
+++ src/components/ui/button/ReleaseCard.tsx
@@ -6,11 +6,12 @@ import Link from 'next/link';
 import styles from './ReleaseCard.module.css';
 import { InferAttributes } from 'sequelize';
 import CoverImage from '../CoverImage';
+import { getUrlBase } from '@/app/(main)/audio/release/[id]/page';
 
 export default function ReleaseCard({ release }: { release: InferAttributes<Release> }) {
     return <Link className={styles.releaseCardLink} href={`/audio/release/${release.id}`}>
         <button className={styles.releaseCard}>
-            <CoverImage src={release.cover_url} alt={`${release.name} cover`} size={200} style={ { maxWidth: '220px' } }/>
+            <CoverImage src={getUrlBase(release.id, release.type, release.size) + 'cover.webp'} alt={`${release.name} cover`} size={200} style={ { maxWidth: '220px' } }/>
             <p style={{ fontWeight: 400 }}>{release.shortname}</p>
             <p style={{ fontSize: '0.8em', opacity: 0.6 }}>
                 {release.release_date ? new Date(release.release_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) : '(TBA)'}<br/>
