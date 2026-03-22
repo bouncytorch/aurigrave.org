4aadcd226b1837ee1477dada596569591a3a3f62
bouncytorch <bouncytorch@aurigrave.org>
2026-03-22 11:22:00 +0100

    Multiple aesthetic and adaptive CSS changes.
    - Converted /audio's ReleaseCardList from flex list to grid
    - Changed ReleaseCard appearance to look less like a button
    - Moved ThemeButton to footer permanently
    - Wrapped all the hover CSS in @media (hover: hover)
    - Fixed inconsistent CoverImage scale

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,13 +2,18 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import ThemeButton from '../ui/button/ThemeButton';
 
 const YEAR = new Date().getFullYear();
 
 export default function Footer() {
     return <footer style={{ textAlign: 'center' }}>
-        made with ♡ by bouncytorch<br/>
-        aurigrave (c) 2023 - {YEAR}<br/>
-        <a href="http://github.com/bouncytorch/aurigrave.org">source on <FontAwesomeIcon icon={faGithub} /> github</a>
+
+        <p style={{ marginBottom: '10px' }}>
+            made with ♡ by bouncytorch<br/>
+            aurigrave (c) 2023 - {YEAR}<br/>
+            <a href="http://github.com/bouncytorch/aurigrave.org">source on <FontAwesomeIcon icon={faGithub} /> github</a>
+        </p>
+        <ThemeButton />
     </footer>;
 }
